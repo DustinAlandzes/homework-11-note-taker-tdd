03d534d7efc423aa5411a661a755e5ce999506f6
f00 <dustinalandzes@drchrono.com>
2020-04-06 22:17:36 -0500

    solution

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+let notes = [];
+
 router.get('/', function(req, res) {
-  res.json({"hello": "world!"});
+  return res.json({"hello": "world!"});
 });
+
+router.get('/api/notes/', (req, res) => {
+  return res.json(notes);
+});
+
+router.post('/api/notes/', (req, res) => {
+  notes.push({id: notes.length + 1, text: req.body.text});
+  return res.json(notes);
+});
+
+router.delete('/api/notes/:id', (req, res) => {
+  notes = notes.filter(note => note.id !== Number(req.params.id));
+  return res.json(notes);
+})
 
 module.exports = router;
