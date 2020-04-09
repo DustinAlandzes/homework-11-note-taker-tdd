fba122be35b558ae1b7d4e79a3c6a18142ace550
f00 <dustinalandzes@drchrono.com>
2020-04-09 18:03:51 -0500

    add db.json with empty array, keep notes in file instead of in memory

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+
 const router = express.Router();
 
-let notes = [];
-
-router.get('/api/notes/', (req, res) => {
-  return res.json(notes);
+router.get('/api/notes/', async (req, res) => {
+  const notes = fs.readFileSync('db.json', 'utf8');
+  return res.json(JSON.parse(notes));
 });
 
 router.post('/api/notes/', (req, res) => {
-  notes.push({id: notes.length + 1, text: req.body.text});
-  return res.json(notes);
+  const {text} = req.body;
+  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  const pushed_notes = [...notes, {id: notes.length + 1, text: text}];
+  fs.writeFileSync('db.json', JSON.stringify(pushed_notes));
+  return res.json(pushed_notes);
 });
 
 router.delete('/api/notes/:id', (req, res) => {
-  notes = notes.filter(note => note.id !== Number(req.params.id));
-  return res.json(notes);
+  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  const filtered_notes = notes.filter(note => note.id !== Number(req.params.id));
+  fs.writeFileSync('db.json', JSON.stringify(filtered_notes));
+  return res.json(filtered_notes);
 })
 
 module.exports = router;
